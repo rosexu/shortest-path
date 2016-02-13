62e339cfc37c005d41a8a1c8168d671906e0a20b
Ziying Zhang <ziyingzhanginc@gmail.com>
2016-02-13 15:39:49 -0800

    Fix build process

--- static/app/app.js
+++ static/app/app.js
@@ -1,7 +1,7 @@
 'use strict';
 
 // Declare app level module which depends on views, and components
-var app = angular.module('myApp', [
+angular.module('myApp', [
   'ngRoute',
   'myApp.view1',
   'myApp.view2',
