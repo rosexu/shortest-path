53ca39b8c7babe67aae8baadb8196bcc4f762c77
Ziying Zhang <ziyingzhanginc@gmail.com>
2016-02-13 13:40:30 -0800

    Initial angular app

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name shortestPathApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the shortestPathApp
+ */
+angular.module('shortestPathApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name shortestPathApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the shortestPathApp
+ */
+angular.module('shortestPathApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
