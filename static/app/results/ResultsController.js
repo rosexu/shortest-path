2c48fe8840a08436d7449dafd517d6db91b891a8
Rose <rosexu1996@yahoo.com>
2016-02-14 10:04:39 -0800

    result rendering sidebar

--- static/app/results/ResultsController.js
+++ static/app/results/ResultsController.js
@@ -1,11 +1,45 @@
 'use strict';
 
-angular.module('myApp.results', []).controller('ResultsController', ['Itinerary', function(Itinerary) {
-	
-	if (!Itinerary.searchedPlaces || !Itinerary.markers) {
-		return;
-	}
+angular.module('myApp.results', []).controller('ResultsController', ['Itinerary', '$scope',
+	function(Itinerary, $scope) {
 
-	console.log('present new info here');
-	console.log(Itinerary.searchedPlaces, Itinerary.markers, Itinerary.tripLegs);
+		if (!Itinerary.searchedPlaces || !Itinerary.markers) {
+			return;
+		}
+
+		$scope.places = Itinerary.searchedPlaces;
+		sortPlaces();
+		$scope.tripLegs = Itinerary.tripLegs;
+		sanitizeTime();
+
+		function sortPlaces() {
+			if(!$scope.tripLegs) return;
+			var sortedPlaces = [];
+			var firstPlace = places.filter(function(pl) {
+				return pl.formatted_address === tripLegs[0].start_loc;
+			});
+			sortedPlaces.push(firstPlace);
+			tripLegs.forEach(function(leg) {
+				var place = places.filter(function(pl) {
+					return pl.formatted_address === leg.end_loc;
+				});
+				sortedPlaces.push(place);
+			});
+			$scope.places = sortedPlaces;
+		}
+
+		function sanitizeTime() {
+			$scope.tripLegs.forEach(function(leg) {
+				var hour = leg.travel_duration / 3600;
+				var temp_min = leg.travel_duration % 3600;
+				var min = temp_min / 60;
+				leg.travel_duration = {
+					hour: Math.floor(hour),
+					min: Math.floor(min),
+				}
+			})
+		}
+
+		console.log('present new info here');
+		console.log(Itinerary.searchedPlaces, Itinerary.markers, Itinerary.tripLegs);
 }]);
