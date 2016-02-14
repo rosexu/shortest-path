9eff527fcfe33c7a318e6f3a2bd09b86c0d4026d
Ziying Zhang <ziyingzhanginc@gmail.com>
2016-02-13 22:11:32 -0800

    Working front-end

--- static/app/app.js
+++ static/app/app.js
@@ -1,12 +1,21 @@
 'use strict';
 
 // Declare app level module which depends on views, and components
-angular.module('myApp', [
-  'ngRoute',
-  'myApp.view1',
-  'myApp.view2',
-  'myApp.version'
-]).
-config(['$routeProvider', function($routeProvider) {
-  $routeProvider.otherwise({redirectTo: '/view1'});
-}]);
+angular.module('myApp', ['ngRoute','myApp.main', 'myApp.results'])
+  .config(['$routeProvider',
+    function($routeProvider) {
+      $routeProvider
+        .when('/', {
+          templateUrl: 'static/app/main/mainView.html',
+          controller: 'MainController'
+        })
+        .when('/results', {
+          templateUrl: 'static/app/results/resultsView.html',
+          controller: 'ResultsController'
+        })
+        .otherwise({
+          redirectTo: '/'
+        });
+    }
+  ]
+);
